b4a7739dfdc87eb4fc1f11c2b29021aa642ba797
Artem Chepurnoy <artemchep@gmail.com>
2020-11-14 13:55:05 +0200

    Use #dontkillmyapp app argument

--- app/src/main/java/com/artemchep/pocketmode/Links.kt
+++ app/src/main/java/com/artemchep/pocketmode/Links.kt
@@ -3,4 +3,4 @@ package com.artemchep.pocketmode
 const val LINK_REPOSITORY = "https://github.com/AChep/pocketmode"
 const val LINK_TRANSLATE = "https://crowdin.com/project/pocketmode"
 const val LINK_BUG_REPORT = "https://github.com/AChep/pocketmode/issues"
-const val LINK_BUG_REPORT_DONT_KILL_MY_APP = "https://dontkillmyapp.com"
+const val LINK_BUG_REPORT_DONT_KILL_MY_APP = "https://dontkillmyapp.com?app=Pocket%20Mode"
