89d4f96aa635e36bc8f7f1117b9e64567edc189b
Artem Chepurnoy <artemchep@gmail.com>
2021-02-14 11:42:49 +0200

    Safely access the firebase instances

--- app/src/playstore/java/com/artemchep/pocketmode/analytics/AnalyticsFirebase.kt
+++ app/src/playstore/java/com/artemchep/pocketmode/analytics/AnalyticsFirebase.kt
@@ -8,34 +8,54 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 fun createAnalytics(application: Application): Analytics =
-    AnalyticsFirebase(FirebaseAnalytics.getInstance(application))
+    AnalyticsFirebase(application)
 
 /**
  * @author Artem Chepurnoy
  */
 private class AnalyticsFirebase(
-    private val analytics: FirebaseAnalytics
+    private val application: Application
 ) : Analytics {
 
     private val deviceInfo by lazy { "device" to Build.MODEL }
 
+    private var analytics: FirebaseAnalytics? = null
+        get() = field ?: try {
+            FirebaseAnalytics.getInstance(application)
+                .also {
+                    field = it
+                }
+        } catch (e: java.lang.Exception) {
+            null
+        }
+
+    private var crashlytics: FirebaseCrashlytics? = null
+        get() = field ?: try {
+            FirebaseCrashlytics.getInstance()
+                .also {
+                    field = it
+                }
+        } catch (e: java.lang.Exception) {
+            null
+        }
+
     override fun log(message: String) {
-        FirebaseCrashlytics.getInstance().log(message)
+        crashlytics?.log(message)
     }
 
     override fun logPocketModeTriggered() {
         val bundle = bundleOf(deviceInfo)
-        analytics.logEvent(AnalyticsEvent.POCKET_MODE_TRIGGERED.name, bundle)
+        analytics?.logEvent(AnalyticsEvent.POCKET_MODE_TRIGGERED.name, bundle)
     }
 
     override fun logPocketModeLocked() {
         val bundle = bundleOf(deviceInfo)
-        analytics.logEvent(AnalyticsEvent.POCKET_MODE_LOCKED.name, bundle)
+        analytics?.logEvent(AnalyticsEvent.POCKET_MODE_LOCKED.name, bundle)
     }
 
     override fun logTestProximitySensorChange(proximity: Proximity) {
         val bundle = bundleOf(deviceInfo)
-        analytics.logEvent(AnalyticsEvent.PROXIMITY_TEST.name, bundle)
+        analytics?.logEvent(AnalyticsEvent.PROXIMITY_TEST.name, bundle)
     }
 
 }
